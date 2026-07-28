41601997c64b83e06e72f3961ad31fa1de8185b5
Buğra Mete Tura <bugratura11x@gmail.com>
2026-07-28 13:59:19 +0300

    Otopark_Takip_Sistemi

---
+++ Otopark_Takip/Otopark_Takip/Program.cs
@@ -0,0 +1,127 @@
+﻿namespace Otopark_Takip
+{
+    internal class Program
+    {
+        static int toplamYer = 40;
+        static int bosYer = 40;
+        static int toplamAracSayisi = 0;
+
+        static int[] iceridekiAraclar = new int[7];
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("===Otopark Takip Sistemi===");
+                Console.WriteLine($"Toplam Yer : {toplamYer} br");
+                Console.WriteLine($"Kalan Boş Yer : {bosYer} br");
+                Console.WriteLine($"İçerideki Araç : {toplamAracSayisi} adet\n");
+
+                Console.WriteLine("1 - Araç Girişi Yap");
+                Console.WriteLine("2 - Araç Çıkışı  Yap");
+                Console.WriteLine("3 - Sistemi Kapat");
+                Console.Write("\nİşlem Seçiniz:  ");
+
+                string secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                    case "1":
+                        AracIslemi(true);
+                        break;
+
+                    case "2":
+                        AracIslemi(false);
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Sistem Kapatılıyor...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Geçersiz seçim! Devam etmek için bir tuşa basın...");
+                        Console.ReadKey();
+                        break;
+
+                }
+            }
+        }
+
+        static void AracIslemi(bool girisMi)
+        {
+            Console.Clear();
+            Console.WriteLine(girisMi ? "___ Araç Girişi ___ " : "___ Araç Çıkışı ___");
+            Console.WriteLine("1 - Motosiklet (1 br) ");
+            Console.WriteLine("2 - Araba - Sedan (2 br) ");
+            Console.WriteLine("3 - Araba - SUV (3 br) ");
+            Console.WriteLine("4 - Kamyonet (4 br) ");
+            Console.WriteLine("5 - Kamyon (5 br) ");
+            Console.WriteLine("6 - Tır (6 br)  ");
+            Console.WriteLine("0 - İptal ve Ana Menüye Dön");
+            Console.Write("\nAraç Tipini Seçiniz: ");
+
+            string tipSecimi = Console.ReadLine();
+            int aracBirim = 0;
+            int tipIndeks = 0;
+
+            switch (tipSecimi)
+            {
+                case "1": aracBirim = 1; tipIndeks = 1; break;
+                case "2": aracBirim = 2; tipIndeks = 2; break;
+                case "3": aracBirim = 3; tipIndeks = 3; break;
+                case "4": aracBirim = 4; tipIndeks = 4; break;
+                case "5": aracBirim = 5; tipIndeks = 5; break;
+                case "6": aracBirim = 6; tipIndeks = 6; break;
+                case "0": return;
+                default:
+                    Console.WriteLine("Hatalı araç tipi seçtiniz.Devam etmek için herhangi bir tuşa basınız...");
+                    Console.ReadKey();
+                    return;
+            }
+            if (girisMi)
+            {
+                if (bosYer >= aracBirim)
+                {
+                    bosYer -= aracBirim;
+                    toplamAracSayisi++;
+                    iceridekiAraclar[tipIndeks]++;
+                    Console.WriteLine($"\nGiriş başarılı! Bu araç {aracBirim} bir yer kapladı.");
+                    Console.WriteLine($"Otoparkta kalan yer: {bosYer} br");
+                }
+                else
+                {
+                    Console.WriteLine("\nUyarı: Otoparkta yeterli alan yok!");
+                }
+            }
+            else {
+
+                    if (iceridekiAraclar[tipIndeks] > 0)
+                    {
+                        bosYer += aracBirim;
+                        toplamAracSayisi--;
+                        iceridekiAraclar[tipIndeks]--;
+                        Console.WriteLine($"\nÇıkış başarılı! {aracBirim} br alan boşaldı. ");
+                        Console.WriteLine($"Otoparkta güncel yer: {bosYer} br");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nHata: Otoparkta bu tipten bir araç bulunmuyor !");
+                    }
+                }
+                Console.WriteLine("\nAna Menüye dönmek için bir tuşa basınız...");
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+            
+    
+
